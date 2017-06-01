23d9413fb22dbe9353051062fddab5a64d9d1db4
wuyi05 <wuyi05@baidu.com>
2017-06-01 17:09:02 +0800

    refactor restclient

--- go/paddlecloud/get.go
+++ go/paddlecloud/get.go
@@ -60,13 +60,9 @@ func (p *GetCommand) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{
 }
 
 func workers(jobname string) error {
-	token, err := token()
-	if err != nil {
-		return err
-	}
 	queryMap := make(map[string]string)
 	queryMap["jobname"] = jobname
-	respBody, err := getCall(config.ActiveConfig.Endpoint+"/api/v1/workers/", queryMap, token)
+	respBody, err := GetCall(config.ActiveConfig.Endpoint+"/api/v1/workers/", queryMap)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error getting workers: %v\n", err)
 		return err
@@ -95,11 +91,7 @@ func workers(jobname string) error {
 }
 
 func jobs() error {
-	token, err := token()
-	if err != nil {
-		return err
-	}
-	respBody, err := getCall(config.ActiveConfig.Endpoint+"/api/v1/jobs/", nil, token)
+	respBody, err := GetCall(config.ActiveConfig.Endpoint+"/api/v1/jobs/", nil)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error getting jobs: %v\n", err)
 		return err
@@ -133,11 +125,7 @@ func jobs() error {
 }
 
 func quota() error {
-	token, err := token()
-	if err != nil {
-		return err
-	}
-	respBody, err := getCall(config.ActiveConfig.Endpoint+"/api/v1/quota/", nil, token)
+	respBody, err := GetCall(config.ActiveConfig.Endpoint+"/api/v1/quota/", nil)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error getting quota: %v\n", err)
 		return err
