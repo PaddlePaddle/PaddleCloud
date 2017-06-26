8247b5d4143a92c2bf9499acec4b22f2b815cdaf
Your Name <weibao.gongw@gmail.com>
2017-06-26 20:26:32 +0800

    fix bugs

--- docker/convert/convert.py
+++ docker/convert/convert.py
@@ -20,34 +20,7 @@ def convert(output_path, name):
     path = output_path + "/" + name
     mkdir_p(path)
 
-    if  name == 'mq2007' or name == "sentiment":
-        ds.common.convert(path, mod.train, 10, name + "_train")
-        ds.common.convert(path, mod.test, 10, name + "_test")
-    elif name == 'conll05':
-        ds.common.convert(path, mod.test(), 10, name + "_train")
-        ds.common.convert(path, mod.test(), 10, name + "_test")
-    elif name == 'imdb':
-        word_dict = ds.imdb.word_dict()
-        ds.common.convert(path, lambda:mod.train(word_dict), 10, name + "_train")
-        ds.common.convert(path, lambda:mod.test(word_dict), 10, name + "_test")
-    elif name == 'imikolov':
-        N=5
-        word_dict = ds.imikolov.build_dict()
-        ds.common.convert(path, mod.train(word_dict, N), 10, name + "_train")
-        ds.common.convert(path, mod.test(word_dict, N), 10, name + "_test")
-    elif name == 'cifar':
-        ds.common.convert(path, mod.train100(), 10, name + "_train100")
-        ds.common.convert(path, mod.test100(), 10, name + "_test100")
-        ds.common.convert(path, mod.train10(), 10, name + "_train10")
-        ds.common.convert(path, mod.test10(), 10, name + "_test10")
-    elif name == 'wmt14':
-        dict_size = 30000
-        ds.common.convert(path, mod.train(dict_size), 10, name + "_train")
-        ds.common.convert(path, mod.test(dict_size), 10, name + "_test")
-    else:
-        ds.common.convert(path, mod.train(), 10, name + "_train")
-        ds.common.convert(path, mod.test(), 10, name + "_test")
-
+    mod.convert(path)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
