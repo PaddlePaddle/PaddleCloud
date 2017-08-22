371a845a5b09f4fa7ed57d76a500712642a7901b
typhoonzero <typhoonzero1986@gmail.com>
2017-08-22 14:31:59 +0800

    update docker build script

--- docker/build_job_image.sh
+++ docker/build_job_image.sh
@@ -2,15 +2,33 @@
 cur_path="$(cd "$(dirname "$0")" && pwd -P)"
 WHL_URL="https://pypi.python.org/packages/3b/74/2ae16bfac4bc3947f20347852e111a64758947dcbd24b2bdb8540319624c/paddlepaddle-0.10.0-cp27-cp27mu-manylinux1_x86_64.whl#md5=7550b01af5f75939ea72346de8735c6d"
 
+USAGE="$0 cpu|gpu [paddle cloud job image name]"
+
+if [ -n "$1" ]; then
+  if [ "$1" == "cpu" ]; then
+    BASE_IMAGE="ubuntu:16.04"
+  elif [ "$1" == "gpu" ]; then
+    BASE_IMAGE="nvidia/cuda:8.0-cudnn5-runtime-ubuntu16.04"
+  else
+    echo "first argument must be cpu or gpu"
+    exit 1
+  fi
+else
+  echo "usage: " $USAGE
+  exit 1
+fi
+
+echo ${BASE_IMAGE}
+
 #PaddleCloud job Docker image
-if [ ! -n "$1" ]; then
+if [ ! -n "$2" ]; then
   pcloudjob_image=paddlepaddle/cloud-job
 else
-  pcloudjob_image=$1
+  pcloudjob_image=$2
 fi
 
 # find if local whl package exists
-WHL_PACKAGE=$(ls paddlepaddle-*.whl)
+WHL_PACKAGE=$(ls paddlepaddle*.whl)
 if [ $? -ne 0 ]; then
   wget -q $WHL_URL
   if [ $? -ne 0 ]; then
@@ -23,12 +41,14 @@ echo "pcloudjob_image": $pcloudjob_image
 
 #Build Docker Image
 cat > Dockerfile <<EOF
-FROM python:2.7-stretch
+FROM $BASE_IMAGE
+RUN /bin/bash -c 'sed -i 's#http://archive.ubuntu.com/ubuntu#http://mirrors.ustc.edu.cn/ubuntu#g' /etc/apt/sources.list;'
+RUN apt-get update -y && \
+  apt-get install -y python-pip iputils-ping libgtk2.0-dev && \
+  apt-get install -f -y && apt-get clean -y && \
+  pip install -U kubernetes opencv-python
 ADD $WHL_PACKAGE /
-RUN pip install -U kubernetes opencv-python && \
-  pip install /$WHL_PACKAGE && \
-  apt-get update -y && \
-  apt-get install -y iputils-ping libgtk2.0-dev 
+RUN pip install /$WHL_PACKAGE
 ADD ./paddle_k8s /usr/bin
 ADD ./k8s_tools.py /root/
 
