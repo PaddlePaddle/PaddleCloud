8f602e7a06812a3e3a8d65faef55c2425f1f19c9
typhoonzero <typhoonzero1986@gmail.com>
2017-10-10 20:32:19 +0800

    WIP

---
+++ go/cmd/controller/controller.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"flag"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	k8scontroller "github.com/PaddlePaddle/cloud/go/controller"
+)
+
+func newClient(cfg *rest.Config) (*rest.RESTClient, *runtime.Scheme, error) {
+	scheme := runtime.NewScheme()
+	if err := crv1.AddToScheme(scheme); err != nil {
+		return nil, nil, err
+	}
+
+	config := *cfg
+	config.GroupVersion = &crv1.SchemeGroupVersion
+	config.APIPath = "/apis"
+	config.ContentType = runtime.ContentTypeJSON
+	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: serializer.NewCodecFactory(scheme)}
+
+	client, err := rest.RESTClientFor(&config)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return client, scheme, nil
+}
+
+func main() {
+	kubeconfig := flag.String("kubeconfig", "", "Path to a kube config. Only required if out-of-cluster.")
+	flag.Parse()
+
+	// Create the client config. Use kubeconfig if given, otherwise assume in-cluster.
+	config, err := buildConfig(*kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	// make a new config for our extension's API group, using the first config as a baseline
+	client, scheme, err := newClient(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// start a controller on instances of our custom resource
+	controller := k8scontroller.Controller{
+		Client: client,
+		Scheme: scheme,
+	}
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	go controller.Run(ctx)
+}
+
+func buildConfig(kubeconfig string) (*rest.Config, error) {
+	if kubeconfig != "" {
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+	return rest.InClusterConfig()
+}
---
+++ go/controller/controller.go
@@ -0,0 +1,85 @@
+/* Copyright (c) 2016 PaddlePaddle Authors All Rights Reserve.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+	 limitations under the License. */
+
+package controller
+
+import (
+	"context"
+
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+)
+
+// Controller for dispatching TrainingJob resource.
+type Controller struct {
+	client *rest.RESTClient
+	scheme *runtime.Scheme
+}
+
+// Run start to watch kubernetes events and do handlers.
+func (c *Controller) Run(ctx context.Context) error {
+	err := c.startWatch(ctx)
+	if err != nil {
+		return err
+	}
+	<-ctx.Done()
+	return ctx.Err()
+}
+
+func (c *Controller) startWatch(ctx context.Context) error {
+	source := cache.NewListWatchFromClient(
+		c.client,
+		TrainingJobs,
+		apiv1.NamespaceAll,
+		fields.Everything())
+
+	_, informer := cache.NewInformer(
+		source,
+		&TrainingJob{},
+
+		// resyncPeriod
+		// Every resyncPeriod, all resources in the cache will retrigger events.
+		// Set to 0 to disable the resync.
+		0,
+
+		// TrainingJob custom resource event handlers.
+		cache.ResourceEventHandlerFuncs{
+			AddFunc:    c.onAdd,
+			UpdateFunc: c.onUpdate,
+			DeleteFunc: c.onDelete,
+		})
+
+	go informer.Run(ctx.Done())
+	return nil
+}
+
+func (c *Controller) onAdd(obj interface{}) {
+	job := obj.(*TrainingJob)
+	// call c.client.Put() to send REST call to api-server
+}
+
+func (c *Controller) onUpdate(oldObj, newObj interface{}) {
+	oldjob := oldObj.(*TrainingJob)
+	newjob := newObj.(*TrainingJob)
+	// call c.client.Put() to update resource
+}
+
+func (c *Controller) onDelete(obj interface{}) {
+	job := obj.(*TrainingJob)
+	// call c.client.Delete()
+
+}
