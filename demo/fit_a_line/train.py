ced8b7bc3f6b393127c24de0f59211aa282c0add
Yancey <yancey1989@gmail.com>
2017-10-12 21:10:52 +0800

    Add cluster RecordIO files reader (#387)
    Fix fitaline demo

--- demo/fit_a_line/train.py
+++ demo/fit_a_line/train.py
@@ -2,38 +2,58 @@ import paddle.v2 as paddle
 import paddle.v2.dataset as dataset
 import os
 import gzip
+import sys
 
-#PaddleCloud cached the dataset on /pfs/${DATACENTER}/public/dataset/...
-dc = os.getenv("PADDLE_CLOUD_CURRENT_DATACENTER")
-dataset.common.DATA_HOME = "/pfs/%s/public/dataset" % dc
+# NOTE: You should full fill your username, for example:
+#   USERNAME = "paddle@example.com"
+# TODO(Yancey1989): fetch username from environment variable.
+USERNAME = "YOUR USERNAME"
 
-trainer_id = int(os.getenv("PADDLE_INIT_TRAINER_ID"))
-trainer_count = int(os.getenv("PADDLE_INIT_NUM_GRADIENT_SERVERS"))
+DC = os.getenv("PADDLE_CLOUD_CURRENT_DATACENTER")
 
-# TODO(helin): remove this once paddle.v2.reader.creator.recordio is
-# fixed.
-def recordio(paths, buf_size=100):
+#PaddleCloud cached the dataset on /pfs/${DATACENTER}/home/${USERNAME}/...
+dataset.common.DATA_HOME = "/pfs/%s/home/%s" % (DC, USERNAME)
+TRAIN_FILES_PATH = os.path.join(dataset.common.DATA_HOME, "uci_housing")
+
+def prepare_dataset():
+    dataset.common.convert(TRAIN_FILES_PATH,
+                           dataset.uci_housing.train(), 100, "train")
+
+TRAINER_ID = int(os.getenv("PADDLE_INIT_TRAINER_ID"))
+TRAINER_INSTANCES = int(os.getenv("PADDLE_INIT_NUM_GRADIENT_SERVERS"))
+
+def cluster_reader_recordio(paths, trainer_id, trainer_instances):
     """
-    Creates a data reader from given RecordIO file paths separated by ",",
-        glob pattern is supported.
-    :path: path of recordio files.
-    :returns: data reader of recordio files.
+    Creates a cluster data reader from given RecordIO file paths,
+        each trainer will read a subset of the whole files set.
+
+    :paths: path of recordio files.
+    :trainer_id: current trainer ID.
+    :trainer_instances: total trainer instances count.
+    :returns data reader of RecordIO files.
     """
 
     import recordio as rec
-    import paddle.v2.reader.decorator as dec
-    import cPickle as pickle
+    import pickle
+    import glob
 
     def reader():
-        f = rec.reader(paths)
-        while True:
-            r = f.read()
-            if r is None:
-                break
-            yield pickle.loads(r)
-        f.close()
-
-    return dec.buffered(reader, buf_size)
+        file_list = glob.glob(paths)
+        file_list.sort()
+        my_file_list = []
+        # collect a subset files according with the trainer_id
+        for idx, f in enumerate(file_list):
+            if idx % trainer_instances == trainer_id:
+                my_file_list.append(f)
+        for f in my_file_list:
+            print "processing", f
+            reader = rec.reader(f)
+            record_raw = reader.read()
+            while record_raw:
+                yield pickle.loads(record_raw)
+                record_raw = reader.read()
+            reader.close()
+    return reader
 
 def main():
     # init
@@ -68,14 +88,19 @@ def main():
                 reader=paddle.batch(dataset.uci_housing.test(), batch_size=2),
                 feeding=feeding)
             print "Test %d, Cost %f" % (event.pass_id, result.cost)
-            if trainer_id == "0":
+            if TRAINER_ID == "0":
                 with gzip.open("fit-a-line_pass_%05d.tar.gz" % event.pass_id,
                                "w") as f:
                     parameters.to_tar(f)
     # training
     trainer.train(
         reader=paddle.batch(
-            paddle.reader.shuffle(recordio("/pfs/dlnel/public/dataset/uci_housing/uci_housing_train*"), buf_size=500),
+            paddle.reader.shuffle(
+                cluster_reader_recordio(
+                    os.path.join(TRAIN_FILES_PATH, "train-*"),
+                    TRAINER_ID,
+                    TRAINER_INSTANCES),
+                buf_size=500),
             batch_size=2),
         feeding=feeding,
         event_handler=event_handler,
@@ -83,4 +108,16 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    usage = "python train.py [prepare|train]"
+    if len(sys.argv) != 2:
+        print usage
+        exit(1)
+
+    if TRAINER_ID == -1 or TRAINER_INSTANCES == -1:
+        print "no cloud environ found, must run on cloud"
+        exit(1)
+
+    if sys.argv[1] == "prepare":
+        prepare_dataset()
+    elif sys.argv[1] == "train":
+        main()
