e8673b65cdd0423d195af5451cbc0828980d2303
Yancey1989 <yancey1989@gmail.com>
2017-10-27 11:18:20 +0800

    update

--- doc/autoscale/experiment/python/collector.py
+++ doc/autoscale/experiment/python/collector.py
@@ -16,14 +16,15 @@ class JobInfo(object):
         self.submit_time = 0
         self.start_time = 0
         self.end_time = 0
+        self.parallelism = 0
 
     def status_str(self):
         if self.status == JOB_STATUS_FINISHED:
-            return 'FINISHED'
+            return 'FINISH'
         elif self.status == JOB_STATUS_PENDING:
             return 'PENDING'
         elif self.status == JOB_STATUS_NOT_EXISTS:
-            return 'NOT_EXISTS'
+            return 'WAITING'
         elif self.status == JOB_STATUS_RUNNING:
             return 'RUNNING'
 
@@ -83,49 +84,60 @@ class Collector(object):
     def cpu_utils(self):
         cpu = 0
         for item in self._pods:
+            if item.status.phase != 'Running':
+                continue 
             for container in item.spec.containers:
                 requests = container.resources.requests
                 if requests:
                     cpu += self._real_cpu(requests.get('cpu', None))
         
-        return '%0.2f' % ((100.0 * cpu) / self.cpu_allocatable)
+        return '%0.2f%%' % ((100.0 * cpu) / self.cpu_allocatable)
 
     def gpu_utils(self):
         gpu = 0
         for item in self._pods:
+            if item.status.phase != 'Running':
+                continue
             for container in item.spec.containers:
-                requests = container.resources.requests
-                if requests:
-                    gpu += int(requests.get('gpu', 0))
+                limits = container.resources.limits
+                if limits:
+                    gpu += int(limits.get('alpha.kubernetes.io/nvidia-gpu',0))
         if not self.gpu_allocatable:
             return '0'
-        return '%0.2f' % ((100.0 * gpu) / self.gpu_allocatable)
+        return '%0.2f%%' % ((100.0 * gpu) / self.gpu_allocatable)
 
-    def update_job(self, job):
+    def update_job(self, job, times):
         phases = set()
+        parallelism = 0
         for item in self._pods:
             if item.metadata.labels: 
                 for k, v in item.metadata.labels.items():
                     # All PaddleCloud jobs has the label key: paddle-job-*
                     if k == 'paddle-job' and v == job.name:
+                        parallelism += 1
                         if not job.submit_time:
-                            job.submit_time = int(time.time())
+                            job.submit_time = times
                         phases.add(item.status.phase)
 
+        job.parallelism = parallelism
         if phases and not job.submit_time:
-            job.submit_time = int(time.time())
+            job.submit_time = times
 
         if len(phases) == 0:
             # The job has not been submited
             return
         elif len(phases) == 1 and 'Running' in phases:
+        #elif 'Running' in phases:
             # If all pods is Running phase, the job is running
+            # TODO(Yancey1989): If fault-tolerant job, only need 
+            # one of the Pod running
             if not job.start_time:
-                job.start_time = int(time.time())
+                job.start_time = times
             job.status = JOB_STATUS_RUNNING
-        elif 'Failed' in phases or \
-            (len(phases) == 1 and 'Succeeded' in phases):
-            job.end_time = int(time.time())
+        elif ('Failed' in phases or \
+            (len(phases) == 1 and 'Succeeded' in phases)) and \
+            job.end_time == 0:
+            job.end_time = times
             job.status = JOB_STATUS_FINISHED
         elif 'Pending' in phases:
             job.status = JOB_STATUS_PENDING
--- doc/autoscale/experiment/python/main.py
+++ doc/autoscale/experiment/python/main.py
@@ -5,27 +5,29 @@ import sys
 import utils
 
 def case1(c, jobs):
-    print 'Times\tName\tStatus\tCPU\tGPU'
+    print 'Times\tName\tStatus\tCPU\tGPU\tPARALLELISM'
     times = 0
     while True:
-        t = c.run_once()
+        c.run_once()
         for job in jobs:
-            c.update_job(job)
-
-            print '%d\t%s\t%s\t%s\t%s' % (times * settings.COLLECTION_INTERVAL,\
-                job.name, job.status_str(), c.cpu_utils(), c.gpu_utils())
+            c.update_job(job, times)
+            
+            print '%d\t%s\t%s\t%s\t%s\t%d' % (times,\
+                job.name, job.status_str(), c.cpu_utils(), c.gpu_utils(), job.parallelism)
 
         if utils.is_all_jobs_finished(jobs):
             print 'Average running time:', utils.avg_running_time(jobs)
             print 'Average pending time:', utils.avg_pending_time(jobs)
             print 'Cluster wide CPU:', c.cpu_allocatable
             print 'Cluster wide GPU:', c.gpu_allocatable
+            for job in jobs:
+                print '%s runnint time:' % job.name, (job.end_time - job.start_time)
             sys.exit(0)
 
         # TODO(Yancey1989): draw the figure with Ploter
 
         time.sleep(settings.COLLECTION_INTERVAL)
-        times += 1
+        times += settings.COLLECTION_INTERVAL
 
 if __name__=="__main__":
     if len(sys.argv) != 3:
