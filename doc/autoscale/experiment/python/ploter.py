d9410d44d97815a803fc2945b5c2accae6c4c5cd
Helin Wang <ustc.harry@gmail.com>
2017-11-03 14:53:37 -0700

    Plotter: support averging inputs on different timestamps

--- doc/autoscale/experiment/python/ploter.py
+++ doc/autoscale/experiment/python/ploter.py
@@ -4,8 +4,93 @@ import matplotlib.ticker as mticker
 import os
 import csv
 
-DATAFILEPATH = os.getenv("DATAPATH", "./ts.txt ./ts1.txt")
+DATAFILEPATH = os.getenv("DATAPATH", "./ts.txt,./ts1.txt")
 CASEID = os.getenv("CASE", "1")
+DATA_MAX = int(os.getenv("DATA_MAX", "9999999"))
+
+def clean_data(data):
+    new_data = []
+    for ins in data:
+        new_ins = []
+        for step in ins:
+            ts = int(step[0])
+            if ts >= 0 and ts < DATA_MAX:
+                new_ins.append(step)
+        new_data.append(new_ins)
+    return new_data
+
+def nearest(l, val):
+    r = l[0]
+    cur_dis = -1
+    for v in l:
+        dis = abs(int(val[0]) - int(v[0]))
+        if cur_dis < 0 or dis < cur_dis:
+            cur_dis = dis
+            r = v
+    return r
+
+def avg(a, weight_a, b, weight_b):
+    if isinstance(a, list):
+        r = []
+        for i, item in enumerate(a):
+            v = avg(item, weight_a, b[i], weight_b)
+            r.append(v)
+            out = r
+    elif isinstance(a, str):
+        if "|" in a:
+            r = []
+            va = a.split("|")
+            vb = b.split("|")
+            total = len(va)
+            if len(vb) > total:
+                total = len(vb)
+
+            for i in range(total):
+                if len(a) > i:
+                    fa = float(va[i])
+                else:
+                    fa = float(va[-1])
+
+                if len(b) > i:
+                    fb = float(vb[i])
+                else:
+                    fb = float(vb[-1])
+
+                f = (fa * weight_a + fb * weight_b) / (weight_a + weight_b)
+                r.append(str(f))
+
+            out = "|".join(r)
+        else:
+            a = float(a)
+            b = float(b)
+            out = (a * weight_a + b * weight_b) / (weight_a + weight_b)
+    else:
+        a = float(a)
+        b = float(b)
+        out = (a * weight_a + b * weight_b) / (weight_a + weight_b)
+
+    return out
+
+def merge_two(a, weight_a, b, weight_b):
+    result = []
+
+    for v in a:
+        near = nearest(b, v)
+        result.append(avg(v, weight_a, near, weight_b))
+
+    for v in b:
+        near = nearest(a, v)
+        result.append(avg(near, weight_a, v, weight_b))
+
+    sorted_result = sorted(result, key=lambda v: int(v[0]))
+    return sorted_result
+
+def merge_data(data):
+    data = clean_data(data)
+    result = data[0]
+    for i in range(len(data)-1):
+        result = merge_two(result, i+1, data[i+1], 1)
+    return result
 
 if __name__ == '__main__':
     column_names = [
@@ -20,18 +105,6 @@ if __name__ == '__main__':
         "running trainers for each job",
         "cpu utils for each job"
     ]
-
-    plots_data = map(lambda x: { 'name': x, 'data':[] }, column_names)
-    def storeAverage (average_store, target_store) :
-        for index, row in enumerate(average_store):
-            if len(row) != 0:
-                mean = np.mean(np.array(row), axis=0)
-                target_store[index]["data"].append(mean)
-
-    def smooth(y, box_pts):
-        box = np.ones(box_pts)/box_pts
-        y_smooth = np.convolve(y, box, mode='same')
-        return y_smooth
 
     # read csv files
     data_csvs = []
@@ -64,68 +137,44 @@ if __name__ == '__main__':
                     break
             previous_row = row
     
-    # add offset to csv file and merge
+    # correct data offest
     data_merged = data_csvs[0]["data"]
+    data_corrected = [None] * len(data_csvs)
     standard_time_of_event = int(data_csvs[0]['time_of_event'])
     print "standard time of event", standard_time_of_event
     for index, data_csv in enumerate(data_csvs):
+        time_offset = int(data_csv['time_of_event']) - standard_time_of_event
+        print "time offset", time_offset
+        data_corrected[index] = [[str(int(x[0])-time_offset)] + x[1:] for x in data_csv['data']]
+
+    data_plot = merge_data(data_corrected)
+    plot_data = [[] for _ in range(len(column_names))]
+
+    for row_idx, row in enumerate(data_plot):
+        for col_idx, item in enumerate(row):
+            if isinstance(item, str):
+                v = item.split("|")
+            else:
+                v = [item]
+            v = np.array(v).astype(np.float)
+            plot_data[col_idx].append(v)
+
+    ax_data = np.array(plot_data[0])
+    fig, axes = plt.subplots(len(plot_data)-1, sharex=True)
+
+    #create charts
+    for index, plot in enumerate(plot_data):
         if index == 0:
             continue
-        time_offset = int(data_csv['time_of_event']) - standard_time_of_event
-        for row in data_csv["data"]:
-            if abs(int(row[0]) - (standard_time_of_event + time_offset))<2:
-                print row[0]
-                print (standard_time_of_event + time_offset)
-                print row
-            row[0] = int(row[0]) + time_offset
-            if row[0] >= 0:
-                data_merged.append(row)
 
-    # deal with dupliated timestamps
-    sorted_csv = sorted(data_merged, key=lambda row: int(row[0]), reverse=False)
-    for i in sorted_csv:
-        pass
-        #print i
-    previous_ts = -1
-    average_data_store = [[] for _ in range(len(column_names))]
-    for row in sorted_csv:
-        for index, item in enumerate(row):
-            if index == 0:
-                #jumped to another ts
-                if item != previous_ts:
-                    #process previous data and append average to main store
-                    storeAverage(average_data_store, plots_data)
-                    average_data_store = [[] for _ in range(len(column_names))]
-                previous_ts = item
-            if isinstance(item, basestring) and "|" in item:
-                item = item.split("|")
-            else:
-                item = [item]
-            average_data_store[index].append(np.array(item).astype(np.float))
-    storeAverage(average_data_store, plots_data)
-
-    #timestamp as x axes
-    ax_data = np.array(plots_data[0]["data"]).flatten()
-    plots_data = plots_data[1:]
-    fig, axes = plt.subplots(len(plots_data), sharex=True)
-    #create charts
-    for index, plot in enumerate(plots_data):
-        print plot["name"]
-        ax = axes[index]
-        subplots = np.rot90(plot["data"])
-        #add average line for cpu util chart
-        if plot["name"] == "cpu util":
-            average_arr = np.empty(len(subplots[0]))
-            average_arr.fill(np.average(subplots[0]))
-            subplots = np.append(subplots, [average_arr], axis=0)
-
-        for subplot in subplots:
-            #print subplot
-            y = smooth(subplot, 12)
-            ax.plot(ax_data, y)
+        plot = np.array(plot)
+        name = column_names[index]
+        ax = axes[index-1]
+        ax.plot(ax_data, plot)
+        
         ax.set_ylim(bottom=0)
         ax.yaxis.set_major_locator(mticker.MaxNLocator(4, integer=True))
-        ax.set_title(plot["name"])
+        ax.set_title(name)
 
     plt.subplots_adjust(left=0.07, bottom=0.11, right=0.96, top=0.93, wspace=0.2, hspace=0.57)
     plt.show()
