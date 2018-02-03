e5455d480d9f532331f04bae1595b2f392880790
Yi Wang <yiwang01@baidu.com>
2018-02-03 14:09:30 -0800

    Change to use the updated deepcopy generation program

--- go/edl/resource/deepcopy_gen.sh
+++ go/edl/resource/deepcopy_gen.sh
@@ -3,6 +3,5 @@
 # FIXME: should run this script before build, when using api >= 1.7
 #        api == 1.6 is not compatible with this deep copy code generations.
 
-go get -u k8s.io/gengo
-go build -o /tmp/deepcopy-gen k8s.io/gengo/examples/deepcopy-gen
-/tmp/deepcopy-gen -i github.com/PaddlePaddle/cloud/go/edl/resource -O zz_generated.deepcopy 2> /dev/null
+go get -u github.com/kubernetes/gengo/examples/deepcopy-gen &&
+go run $GOPATH/src/github.com/kubernetes/gengo/examples/deepcopy-gen/main.go -i github.com/PaddlePaddle/cloud/go/edl/resource -O generated.deepcopy
