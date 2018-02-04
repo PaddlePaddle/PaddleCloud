73856da9fb4d43dfc69c65a27a4657aa4d9b555f
Yi Wang <yi.wang.2005@gmail.com>
2018-02-03 21:44:35 -0800

    Remove deepcopy_gen.sh and generated.deepcopy.go (zz_generated.deepcopy.go)

--- go/edl/resource/deepcopy_gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# FIXME: should run this script before build, when using api >= 1.7
-#        api == 1.6 is not compatible with this deep copy code generations.
-
-go get -u github.com/kubernetes/gengo/examples/deepcopy-gen &&
-go run $GOPATH/src/github.com/kubernetes/gengo/examples/deepcopy-gen/main.go -i github.com/PaddlePaddle/cloud/go/edl/resource -O generated.deepcopy
