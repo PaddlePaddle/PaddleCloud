122c55ad5ea6736aa9f643f0bc5909c0f0dd9916
m3ngyang <yangm3ng@gmail.com>
2018-02-27 20:03:39 +0800

    change api version of crd to v1

--- go/apis/paddlepaddle/v1/doc.go
+++ go/apis/paddlepaddle/v1/doc.go
@@ -16,6 +16,6 @@ limitations under the License.
 
 // +k8s:deepcopy-gen=package
 
-// Package v1alpha1 is the v1alpha1 version of the API.
+// Package v1 is the v1 version of the API.
 // +groupName=paddlepaddle.org
-package v1alpha1
+package v1
