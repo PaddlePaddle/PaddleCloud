6f3a371d1dbc97781868c16628b298c7962c1af0
Helin Wang <ustc.harry@gmail.com>
2018-03-09 12:07:29 -0800

    Remove EDL source code from cloud (EDL has already been moved to PaddlePaddle/edl)
    EDL is moved to a new repo because it will be donated to the Linux Foundation.

--- go/edl/resource/deepcopy_gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# FIXME: should run this script before build, when using api >= 1.7
-#        api == 1.6 is not compatible with this deep copy code generations.
-
-go get -u k8s.io/gengo
-go build -o /tmp/deepcopy-gen k8s.io/gengo/examples/deepcopy-gen
-/tmp/deepcopy-gen -i github.com/PaddlePaddle/cloud/go/edl/resource -O zz_generated.deepcopy 2> /dev/null
